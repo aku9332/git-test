a3cd50bc89a9ef1d4407ae518a095ee6c95f50dd
Taixu <Taixu@aku.com>
2021-11-03 14:32:37 +0800

    push commit

--- src/main/java/com/aku/git/GitTest.java
+++ src/main/java/com/aku/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("紧急修复成功");
         System.out.println("快去请如来佛祖");
         System.out.println("紧急修复成功2");
+        System.out.println("push test");
     }
 }
